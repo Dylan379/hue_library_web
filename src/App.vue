ead12230a5a1ffbc8371dcce20d13a3383c3f45c
dylan379 <dylanwong824@gmail.com>
2023-03-06 20:04:49 +0800

    'init'

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class=''>
+    helloworld
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive, toRefs, onBeforeMount, onMounted, watchEffect, computed } from 'vue';
+</script>
+<style scoped lang='less'>
+</style>
